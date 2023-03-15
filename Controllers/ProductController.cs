3ebb3a6b3fe831da287859ffa87f9364454b605a
Yusuf Öz <100285819+yusufozdev@users.noreply.github.com>
2023-03-15 21:36:02 +0300

    Ürünler bölümünde ekle sil işlemleri

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -13,18 +13,32 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         Context c = new Context();
         public ActionResult Index()
         {
-            var urunler = c.Products.ToList();
+            var urunler = c.Products.Where(x => x.IsStock == true).ToList();
             return View(urunler);
         }
         [HttpGet]
         public ActionResult YeniUrun() 
         {
+            List<SelectListItem> deger1 = (from x in c.Categories.ToList() select new SelectListItem
+            {
+                Text = x.CategoryName,
+                Value = x.CategoryID.ToString()
+            }).ToList();
+            ViewBag.dgr1 = deger1;
             return View();
         }
         [HttpPost]
         public ActionResult YeniUrun(Product p)
         { 
             c.Products.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UrunSil(int id) 
+        {
+            var deger = c.Products.Find(id);
+            deger.IsStock = false;
             c.SaveChanges();
             return RedirectToAction("Index");
         }
